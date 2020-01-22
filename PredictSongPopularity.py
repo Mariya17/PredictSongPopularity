96816453779805339d578a041cc794501db1caa2
MusProject <portnoymariya@gmail.com>
2020-01-22 15:18:24 +0200

    prediction and learning

--- PredictSongPopularity.py
+++ PredictSongPopularity.py
@@ -9,21 +9,20 @@ import Measurements
 class PredictSongPopularity:
     def __init__(self):
         self.ordered_list = [
-                             'tempo',
-				        	 'audio_mode',
-				        	 'song_duration_ms',
-				        	 'key',
-				        	 'time_signature',
-				        	 'acousticness',
-				        	 'instrumentalness',
-				        	 'liveness','loudness',
-				        	 'speechiness',
-				        	 'audio_valence',
-				        	 'danceability',
-				        	 'energy',
-                             'song_popularity']
-
-
+            'tempo',
+            'audio_mode',
+            'song_duration_ms',
+            'key',
+            'time_signature',
+            'acousticness',
+            'instrumentalness',
+            'liveness','loudness',
+            'speechiness',
+            'audio_valence',
+            'danceability',
+            'energy',
+            'song_popularity']
+        self.K2_results_file_name = 'DAG_File.csv'
         self.db_file_name = 'song_data.csv'
         self.predicted_results_file_name = 'predicted_results.csv'
         self.predicted_single_song_result_file_name = 'predicted_single_song_result.csv'
@@ -31,9 +30,11 @@ class PredictSongPopularity:
 
         self.processed_data_file_name = 'db_after_preprosessing.csv'
         self.DAG = []
+        self.BN = BayesianNetwork.BN(self.DAG)
 
-    def predict(self):
-        print('Started learning\n')
+    ####################### Separated Predicting #######################
+    #     1.
+    def Preprocessing(self):
         data_base = DataPreprocessing.DataPeprocessing(self.db_file_name, self.processed_data_file_name)
         """
         1. 'MeanShirf' - default
@@ -41,25 +42,56 @@ class PredictSongPopularity:
         3. 'Equal Steps'
         """
         data_base.data_preprosessing('Equal Steps')
+    #     2.
+    def PerformK2(self):
         K2Algorithm = k2.K2(self.ordered_list, self.processed_data_file_name)
-
-        # # DAG_uniform_distrebution = 	[('tempo', 'audio_mode'), ('audio_mode', 'song_duration_ms'), ('tempo', 'time_signature'), ('acousticness', 'instrumentalness'), ('song_duration_ms', 'instrumentalness'), ('acousticness', 'loudness'), ('instrumentalness', 'loudness'), ('acousticness', 'speechiness'), ('audio_valence', 'danceability'), ('tempo', 'danceability'), ('speechiness', 'danceability'), ('instrumentalness', 'song_popularity'), ('loudness', 'song_popularity'), ('energy', 'song_popularity')]
-        # DAG_MeanShift = [('tempo', 'audio_mode'), ('audio_mode', 'song_duration_ms'), ('audio_mode', 'time_signature'), ('acousticness', 'instrumentalness'), ('song_duration_ms', 'instrumentalness'), ('acousticness', 'loudness'), ('tempo', 'danceability'), ('audio_valence', 'danceability'), ('speechiness', 'danceability'), ('instrumentalness', 'song_popularity'), ('acousticness', 'song_popularity')]
-        # # self.DAG = DAG_uniform_distrebution
-
-        # DAG_MeanShift = [('tempo', 'audio_mode'), ('audio_mode', 'song_duration_ms'), ('tempo', 'time_signature'),('acousticness', 'instrumentalness'), ('acousticness', 'loudness'), ('instrumentalness', 'loudness'),('speechiness', 'danceability'), ('instrumentalness', 'song_popularity'), ('loudness', 'song_popularity')]
-        #self.DAG = DAG_MeanShift
-
         self.DAG = K2Algorithm.k2()
         self.convertDagToFile()
-        DagTest = self.convertFileToDAG()
-
-        BayesianNetwork.BN(self.DAG, self.processed_data_file_name, self.predicted_results_file_name)
-
+    #    3.def convertFileToDAG(self)
+    #    4.
+    def bayesianLearning(self):
+        self.BN.BNLearning(self.DAG, self.processed_data_file_name)
+    #    5.
+    def bayesianTesting(self):
+        self.BN.BNTesting(self.predicted_results_file_name)
+    #    6.
+    def mseMeasure(self):
         mse = Measurements.mse(self.processed_data_file_name, self.predicted_results_file_name)
         print("MSE is: {0}%".format(mse))
+
+    #    7.
+    def errorInPresents(self):
         errorRate = Measurements.errorRate(self.processed_data_file_name, self.predicted_results_file_name)
         print("Error Rate is: {0}%".format(errorRate))
+
+    # def predict(self):
+        # print('Started learning\n')
+        # data_base = DataPreprocessing.DataPeprocessing(self.db_file_name, self.processed_data_file_name)
+        # """
+        # 1. 'MeanShirf' - default
+        # 2. 'Uniform Distribution'
+        # 3. 'Equal Steps'
+        # """
+        # data_base.data_preprosessing('Equal Steps')
+        # K2Algorithm = k2.K2(self.ordered_list, self.processed_data_file_name)
+        #
+        # # # DAG_uniform_distrebution = 	[('tempo', 'audio_mode'), ('audio_mode', 'song_duration_ms'), ('tempo', 'time_signature'), ('acousticness', 'instrumentalness'), ('song_duration_ms', 'instrumentalness'), ('acousticness', 'loudness'), ('instrumentalness', 'loudness'), ('acousticness', 'speechiness'), ('audio_valence', 'danceability'), ('tempo', 'danceability'), ('speechiness', 'danceability'), ('instrumentalness', 'song_popularity'), ('loudness', 'song_popularity'), ('energy', 'song_popularity')]
+        # # DAG_MeanShift = [('tempo', 'audio_mode'), ('audio_mode', 'song_duration_ms'), ('audio_mode', 'time_signature'), ('acousticness', 'instrumentalness'), ('song_duration_ms', 'instrumentalness'), ('acousticness', 'loudness'), ('tempo', 'danceability'), ('audio_valence', 'danceability'), ('speechiness', 'danceability'), ('instrumentalness', 'song_popularity'), ('acousticness', 'song_popularity')]
+        # # # self.DAG = DAG_uniform_distrebution
+        #
+        # # DAG_MeanShift = [('tempo', 'audio_mode'), ('audio_mode', 'song_duration_ms'), ('tempo', 'time_signature'),('acousticness', 'instrumentalness'), ('acousticness', 'loudness'), ('instrumentalness', 'loudness'),('speechiness', 'danceability'), ('instrumentalness', 'song_popularity'), ('loudness', 'song_popularity')]
+        # #self.DAG = DAG_MeanShift
+        #
+        # self.DAG = K2Algorithm.k2()
+        # self.convertDagToFile()
+        # DagTest = self.convertFileToDAG()
+
+        # BayesianNetwork.BN(self.DAG, self.processed_data_file_name, self.predicted_results_file_name)
+
+        # mse = Measurements.mse(self.processed_data_file_name, self.predicted_results_file_name)
+        # print("MSE is: {0}%".format(mse))
+        # errorRate = Measurements.errorRate(self.processed_data_file_name, self.predicted_results_file_name)
+        # print("Error Rate is: {0}%".format(errorRate))
 
     def convertDagToFile(self):
         parent = []
