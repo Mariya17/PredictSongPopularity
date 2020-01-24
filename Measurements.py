f70b0d9d362ac9a2005072df4da8868f7dd73118
ameir4 <43065837+ameir4@users.noreply.github.com>
2020-01-25 01:09:22 +0200

    chage create graph func

--- Measurements.py
+++ Measurements.py
@@ -1,5 +1,6 @@
 from sklearn.metrics import mean_squared_error
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import Const
 
@@ -42,20 +43,51 @@ def errorRate(exceptedPath, testPredRes):
 
     return errorRate
 
-def createGraph(fileType):
-    x, y = convertFileTofraph(Const.Files.GRAPH, fileType)
-    for i in y:
+# def createGraph(fileType):
+#     x, y = convertFileTofraph(Const.Files.GRAPH, fileType)
+#     for i in y:
+#         i = 100 - int(i)
+#     plt.plot(x, y,
+#              color='green',ע
+#              marker='o',
+#              linestyle='dashed',
+#              linewidth=2,
+#              markersize=4)
+#     plt.title(fileType)
+#     plt.xlabel('div')
+#     plt.ylabel('success')
+#     plt.show()
+
+def createGraph():
+    data = pd.read_csv(Const.Files.GRAPH)
+    rowsCsv, colCsv = data.shape
+    x = [data['x'][i] for i in range(0, rowsCsv)]
+    errorRateList = [data[Const.GraphType.ERROR_RATE][i] for i in range(0, rowsCsv)]
+    mseList = [data[Const.GraphType.MSE][i] for i in range(0, rowsCsv)]
+    for i in errorRateList:
         i = 100 - int(i)
-    plt.plot(x, y,
-             color='green',
-             marker='o',
-             linestyle='dashed',
-             linewidth=2,
-             markersize=4)
-    plt.title(fileType)
-    plt.xlabel('div')
-    plt.ylabel('success')
+    # width of the bars
+    barWidth = 0.1
+
+    # The x position of bars
+    r1 = np.arange(len(x))
+    r2 = [x + barWidth for x in r1]
+
+    # Create blue bars
+    plt.bar(r1, mseList, width=barWidth, color='green', edgecolor='black', capsize=7, label='MSE')
+
+    # Create cyan bars
+    plt.bar(r2, errorRateList, width=barWidth, color='blue', edgecolor='black', capsize=7, label='Accuracy')
+
+    # general layout
+    plt.xticks([r + barWidth for r in range(len(mseList))], x)
+    plt.legend()
+
+    # Show graphic
+    plt.suptitle("Uniform method")
     plt.show()
+
+
 
 def convertFileTofraph(graph_file, fileType):
     data = pd.read_csv(graph_file)
