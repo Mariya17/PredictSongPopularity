3f57ff5eeabaf0945116779d7e7d790beb9ba6b1
ameir4 <43065837+ameir4@users.noreply.github.com>
2020-01-25 01:08:54 +0200

    remove notes

--- BayesianNetwork.py
+++ BayesianNetwork.py
@@ -27,7 +27,6 @@ class BN:
 
         self.model.fit(self.data, BayesianEstimator)
 
-
     def BNTesting(self, results_file):
         # separate data for test
         training_part = int(0.8 * len(self.data))
@@ -40,26 +39,6 @@ class BN:
 
         with open(results_file, 'w', newline='') as file:
             y_pred.to_csv(file)
-
-    # def BN(self, DAG, db_file, results_file):
-
-        # data = take_only_relevant_features(DAG, db_file)
-
-        # training_data = self.data[:15068]
-        # predict_data = self.data[15068:16952]
-        #
-        # model = BayesianModel(DAG)
-        #
-        # model.fit(data, BayesianEstimator)
-
-        # predict_data = predict_data.copy()
-        # predict_data.drop('song_popularity', axis=1, inplace=True)
-        # y_pred = self.model.predict(predict_data)
-        # #print(y_pred)
-        #
-        # with open(results_file, 'w', newline='') as file:
-        #     y_pred.to_csv(file)
-
 
     def BNForOneSong(self, DAG, db_file, results_file, songFile):
         data = self.take_only_relevant_features(DAG, db_file)
